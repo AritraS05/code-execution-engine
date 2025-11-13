fb29bb22e32820c95e2c5a2312d5a64c1d323814
Aritra <aritrasarkar00007@gmail.com>
2025-11-13 18:18:38 +0530

    added api route

---
+++ src/app/api/submit/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+export async function POST(req: NextRequest){
+    const {code, language, problemId} = await req.json();
+    let verdict = "Accepted", errors:string|null=null,details:string[]=[];
+    if(!code){
+        verdict="Rejected";
+        errors="no code is written";
+        details=["write someting :)"];
+    }
+    else if(code.includes("error")){
+        verdict:"compilation error";
+        errors="an error found in code";
+    }
+
+    await new Promise(res => setTimeout(res,2000));
+    return NextResponse.json({verdict,errors,details});
+}
